1eb282c282d30879f311dff2b398a2d5e7a0456f
Deniz Şara <denizsara@Deniz-MacBook-Air.local>
2026-02-20 01:37:03 +0300

    Fix port for Render deployment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const converter = require('./services/converter');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
